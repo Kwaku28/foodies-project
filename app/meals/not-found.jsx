ed5474b26e3dfd84a8a86295854d1faa162aad66
Lawrence Amoafo <rekenburg777@gmail.com>
2024-01-25 11:06:31 +0000

    Add `not-found` page for when specific meals are not in the database

---
+++ app/meals/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>Try going back to the previous page</p>
+    </main>
+  );
+}
